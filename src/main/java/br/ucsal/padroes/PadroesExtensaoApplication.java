4b530847fb6296a1223a82a886c7eda4a9091211
lucasborgss1 <lucasaborgesdev1@gmail.com>
2026-09-17 21:58:29 -0300

    Cria projeto Spring Boot inicial

---
+++ src/main/java/br/ucsal/padroes/PadroesExtensaoApplication.java
@@ -0,0 +1,12 @@
+package br.ucsal.padroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesExtensaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadroesExtensaoApplication.class, args);
+    }
+}
